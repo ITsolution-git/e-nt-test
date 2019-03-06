af5ff2953be03981eda07c69cc39a8305817475c
Rohit Bhatia <irohitbhatia@gmail.com>
2019-03-06 15:18:30 +0530

    probably configured flamelink

--- index.js
+++ index.js
@@ -6,5 +6,8 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+//Importing FlameLink SDK Configuration
+import flameLinkConfig from "./src/config/flamelink.js"
+
 
 AppRegistry.registerComponent(appName, () => App);
