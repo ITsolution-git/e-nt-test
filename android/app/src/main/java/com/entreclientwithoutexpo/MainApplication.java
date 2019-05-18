71cff3cb768ed3400a6efebed9fe3201dd359521
irohitb <irohitbhatia@gmail.com>
2019-05-18 22:51:56 +0530

    removed flamelink and lottie

--- android/app/src/main/java/com/entreclientwithoutexpo/MainApplication.java
+++ android/app/src/main/java/com/entreclientwithoutexpo/MainApplication.java
@@ -3,7 +3,6 @@ package com.entreclientwithoutexpo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.airbnb.android.react.lottie.LottiePackage;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import io.invertase.firebase.RNFirebasePackage;
@@ -28,7 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new LottiePackage(),
             new ReanimatedPackage(),
             new VectorIconsPackage(),
             new RNFirebasePackage(),
